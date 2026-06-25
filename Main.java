1d3718d65e2c9405c19d75ac59b59c66b0b5af55
LAB4804WD22 <jakezamir@gmail.com>
2026-06-25 10:03:10 -0700

    Creates a method called opinionsOnArticle that writes out my opinions on several topics regarding the ethics of self driving cars and returns the method to the main method.

--- Main.java
+++ Main.java
@@ -1,18 +1,24 @@
 // When you are ready to have your work checked, change the value below from N to Y
 // ReadyForSubmission=N
-// YOUR NAME:
-// COLLABORATORS:
-// DATE:  
-
+// YOUR NAME: Jacob Zamir
+// COLLABORATORS: N/A
+// DATE:  6/25/2026
 public class Main
 {
 	public static void main(String[] args)
 	{
 		System.out.println("My thoughts on the article are:");
 		// Part 2: Invoking/calling method
-		
+		Main.opinionsOnArticle();
 	}
 	
 	// Part 1: Creating method definition
-	
+	// {Method Header: Public static} {Return Type: void} {Method Identifier: opinionsOnArticle} {Parameters: None}
+	public static void opinionsOnArticle()
+	{
+		// Method Body
+		System.out.println("I believe that self driving cars should prioritize the safety of pedestrians because it was the passenger's choice to get in the self driving car and not the pedestrian's.");
+		System.out.println("The car's algorithm should consider factors such as the number of people in the car, the number of pedestrians, the age of pedestrians and passengers, and the best course of action it should take in case something went wrong.");
+		System.out.println("I believe that the law, driving safety instructors, and even lawyers should work in tandem with software engineers to develop the best, safest driving algorithm that will protect the most people in case of an imminent accident.");
+	}
 }
